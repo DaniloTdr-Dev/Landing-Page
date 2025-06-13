d31e48eb2b66c7f6ca98e4e1c036afb445f49275
Danilo <dev.danilotdr@gmail.com>
2025-06-13 14:29:00 -0400

    inicialização da Home

--- src/JavaScript/Script.js
+++ src/JavaScript/Script.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
+    // ao clicar no #mobile_btn, execute a função:
     $('#mobile_btn').on('click', function() {
+
         //para adicionar a classe active quando clicar
         $('#mobile_menu').toggleClass('active');
+        
+        //vai adicionar a classe fa-x na tag <i>, transformando as barras em X
+        $('#mobile_btn').find('i').toggleClass('fa-x');
     })
 })
