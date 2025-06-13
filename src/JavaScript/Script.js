414d1034386b3b949ef031de10ea9f488991dd70
Danilo <dev.danilotdr@gmail.com>
2025-06-13 12:01:20 -0400

    Início da Landing Page
    Finalização do Header

---
+++ src/JavaScript/Script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#mobile_btn').on('click', function() {
+        //para adicionar a classe active quando clicar
+        $('#mobile_menu').toggleClass('active');
+    })
+})
