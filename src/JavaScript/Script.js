1658435a5dd3550905de06e752ab7ad47cbd99a7
Danilo <dev.danilotdr@gmail.com>
2025-06-16 16:57:29 -0400

    Finalização da Landing Page

--- src/JavaScript/Script.js
+++ src/JavaScript/Script.js
@@ -7,5 +7,61 @@ $(document).ready(function() {
         
         //vai adicionar a classe fa-x na tag <i>, transformando as barras em X
         $('#mobile_btn').find('i').toggleClass('fa-x');
-    })
+    });
+
+    // pegando informações do css
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    // quando rolar na janela 
+    $(window).on('scroll', function() {
+        const header = $('header');
+        let activeSecctionIndex = 0;
+        // parametro outerHeight() pega todo o header
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+        
+        if (scrollPosition <= 0) {
+            // se scroll for igual a 0 não coloque sombra
+            header.css('box-shadow', 'none');
+        }
+        else {
+            // caso contrário, coloque essa sombra
+            header.css('box-shadow', '5px 1px 5px rgba(0,0,0,0.1)');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            // parametro outerHeight() pega todo o header
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSecctionIndex = i;
+                return false;
+            }
+        });
+        navItems.removeClass('active');
+        $(navItems[activeSecctionIndex]).addClass('active');
+    });
+
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%',
+    });
+    ScrollReveal().reveal('#dishes', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%',
+    });
+    ScrollReveal().reveal('#testimonial_chef', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%',
+    });
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 2000,
+        distance: '20%',
+    });
 })
